4eaca532ec63f518d0f6ca7cf09c18e1cd049d0a
ondrakodat12 <ondrej.kodat@seznam.cz>
2026-09-18 21:55:46 +0200

    Dneska jen maly commit zas spíše teoretické učení

--- javaScriptUceni.js
+++ javaScriptUceni.js
@@ -4,3 +4,33 @@ function myFunction(){
     const prijmeni = "Kodat";
     document.getElementById("demo").innerHTML = "Jmeno: " + jmeno + "<br> Prijmeni: " + prijmeni;
 }
+
+function pozdrav(){
+    let jmeno = document.getElementById("inputJmeno").value;
+    let delka = jmeno.length;
+    let zmenaJmenaNaPozdrav = jmeno.substring(0,delka-1);
+    let kontrola = kontrolaJmeno(jmeno);
+    if(kontrola == false){
+        return;
+    }
+    
+    let prijmeni = document.getElementById("inputPrijmeni").value;
+    document.getElementById("demo").innerHTML = "Ahoj: " + zmenaJmenaNaPozdrav + "o " + prijmeni + "e.";
+
+}
+
+function kontrolaJmeno(jmeno){
+    let delka = jmeno.length;
+    if (delka < 3){
+        alert("Jmeno je moc kratke");
+        return false;
+    }
+    let obsahujeMezetu = jmeno.incldes(" ");
+    if(obsahujeMezetu){
+        alert("Jmeno nesmi obsahovat mezery");
+        return false;
+    }else {
+        return true;
+    }
+
+}
